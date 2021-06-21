4baccf7c773cebac743997cf758f36ebe3c49262
Zoe <82158799+Zoe8888@users.noreply.github.com>
2021-06-21 15:57:39 +0200

    Update window_2.py

--- window_2.py
+++ window_2.py
@@ -4,8 +4,6 @@ from tkinter import messagebox
 from random import randint
 from playsound import playsound
 
-import main
-
 master = Tk()
 # Creating a title
 master.title('Lotto Generator')
@@ -34,6 +32,7 @@ class UserID:
 class Lotto:
     def __init__(self, root):
         self.root = root
+        self.count = 0
 
         self.select = Label(root, text='Select your lucky Lotto numbers:', fg='#506352', bg='#4ad66d', font=20)
         self.select.place(relx=0.2, rely=0.2)
@@ -84,12 +83,12 @@ class Lotto:
         self.play_again = Button(root, text='Play Again', fg='#506352', bg='#47b553', command=self.replay)
         self.play_again.place(relx=0.2, rely=0.8)
 
-        self.check_numbers = Button(root, text='Check Winnings', fg='#506352', bg='#47b553',
+        self.check_numbers = Button(root, text='Are you a winner?', fg='#506352', bg='#47b553',
                                     command=self.check_lotto_numbers)
         self.check_numbers.place(relx=0.39, rely=0.8)
 
-        self.claim = Button(root, text='Claim Prize', fg='#506352', bg='#47b553', command=self.claim)
-        self.claim.place(relx=0.64, rely=0.8)
+        self.claim_prize = Button(root, text='Claim Prize', fg='#506352', bg='#47b553', command=self.claim)
+        self.claim_prize.place(relx=0.64, rely=0.8)
 
         self.total_lists = []
 
@@ -144,32 +143,10 @@ class Lotto:
 
             self.reveal_lotto.config(state='normal')
 
-    # def replay(self, entry_list):
-    #     entry_list.append([self.num1.get(), self.num2.get(), self.num3.get(), self.num4.get(), self.num5.get(),
-    #                        self.num6.get()])
-    #     print(entry_list)
-    #     yes_no = messagebox.askyesno(message='Are you sure you would like to play again?')
-    #     if yes_no:
-    #         self.num1.delete(0, 'end')
-    #         self.num2.delete(0, 'end')
-    #         self.num3.delete(0, 'end')
-    #         self.num4.delete(0, 'end')
-    #         self.num5.delete(0, 'end')
-    #         self.num6.delete(0, 'end')
-    #
-    #         self.num1.insert(0, 1)
-    #         self.num2.insert(0, 1)
-    #         self.num3.insert(0, 1)
-    #         self.num4.insert(0, 1)
-    #         self.num5.insert(0, 1)
-    #         self.num6.insert(0, 1)
-    #
-    #     else:
-    #         pass
-
     def generate_lotto(self):
         game_lists = [self.num1.get(), self.num2.get(), self.num3.get(), self.num4.get(), self.num5.get(),
                       self.num6.get()]
+        self.total_lists.clear()
         self.total_lists.append(game_lists)
         print(self.total_lists)
 
@@ -208,42 +185,45 @@ class Lotto:
         playsound('drum-roll.mp3')
 
     def check_lotto_numbers(self):
+        if str(self.num1.get()) == str(self.lotto1.get()):
+            self.count = self.count + 1
+        if str(self.num2.get()) == str(self.lotto2.get()):
+            self.count = self.count + 1
+        if str(self.num3.get()) == str(self.lotto3.get()):
+            self.count = self.count + 1
+        if str(self.num4.get()) == str(self.lotto4.get()):
+            self.count = self.count + 1
+        if str(self.num5.get()) == str(self.lotto5.get()):
+            self.count = self.count + 1
+        if str(self.num6.get()) == str(self.lotto6.get()):
+            self.count = self.count + 1
         try:
-            total_winnings = 0
-            for x in range(len(self.total_wins)):
-                count = 0
-                _set = self.total_wins[x]
-                for y in range(len(set)):
-                    if _set[y] == self.lotto_numbers[y]:
-                        count += 1
+            file = open('prize_money.txt', 'a')
+            if self.count == 1:
+                messagebox.showinfo(message="You got 1 correct number. Unfortunately you have not won anything.")
+                file.write('Total 0 \n')
+            elif self.count == 2:
+                messagebox.showinfo(message='You got 2 correct numbers. You have won R20!')
+                file.write('Total 20 \n')
+            elif self.count == 3:
+                messagebox.showinfo(message='You got 3 numbers correct. You have won R100,50!')
+                file.write('Total 100,50 \n')
+            elif self.count == 4:
+                messagebox.showinfo(message='You got 4 numbers correct. You have won R2384!')
+                file.write('Total 2384 \n')
+            elif self.count == 5:
+                messagebox.showinfo(message='You got 5 numbers correct. You have won R8584!')
+                file.write('Total 8584 \n')
+            elif self.count == 6:
+                messagebox.showinfo(message='CONGRATULATIONS! You won the Lottery. You just earned R10 000 000!')
+                file.write('Total 10000000 \n')
+            else:
+                messagebox.showinfo(message='You got no numbers correct. You have not won anything.')
+                file.write('Total 0 \n')
+            file.close()
 
-                    winnings_breakdown = {0: 0, 1: 0, 2: 20, 3: 100.50, 4: 2384, 5: 8584, 6: 10000000}
-                    total_winnings += winnings_breakdown[count]
-                    win = {winnings_breakdown[count]}
-                    self.text_to_file(win)
-
-                    messagebox.showinfo(message='You have won R{}'.format(total_winnings))
         except ValueError:
             messagebox.showerror(message='Make sure you entered a number')
-
-    def text_to_file(self, text):
-        with open('prize_money.txt', 'a') as prize:
-            prize.write(str(text))
-
-            # if count == 1:
-            #     messagebox.showinfo(message="You only got 1 correct number. Unfortunately you don't win anything.")
-            # elif count == 2:
-            #     messagebox.showinfo(message='You got 2 correct numbers. You have won R20!')
-            # elif count == 3:
-            #     messagebox.showinfo(message='You got 3 numbers correct. You have won R100,50!')
-            # elif count == 4:
-            #     messagebox.showinfo(message='You got 4 numbers correct. You have won R2384!')
-            # elif count == 5:
-            #     messagebox.showinfo(message='You got 5 numbers correct. You have won R8584!')
-            # elif count == 6:
-            #     messagebox.showinfo(message='CONGRATULATIONS! You won the lottery. You just earned R10 000 000!')
-            # else:
-            #     messagebox.showinfo(message='Unfortunately you have not won anything.')
 
     def claim(self):
         self.root.destroy()
